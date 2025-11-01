d1a11a7c6ca922ebf9ba56c57eb1e410a7edca2e
Bastian Barrios <andresbarriosmedina1@gmail.com>
2025-11-01 00:53:46 -0300

    version 2.4

--- app/components/layoutManager.tsx
+++ app/components/layoutManager.tsx
@@ -8,14 +8,19 @@ import NavStick from './NavStick';
 
 export default function LayoutManager({ children }: { children: ReactNode }) {
   const pathname = usePathname();
-  const showMainLayout = !pathname.includes('/dashboard');
+  const showMainLayout = !pathname.includes('/dashboard') && pathname !== '/login' && pathname !== '/signup';
 
   return (
     <div className="relative flex flex-col min-h-screen">
+      {/* HEADER */}
       {showMainLayout && <Nav />}
-      <main className="flex-1 overflow-auto pb-24 md:pb-safe">
+
+      {/* CONTENIDO PRINCIPAL */}
+      <main className="flex-1 overflow-auto pb-20 md:pb-safe">
         {children}
       </main>
+
+      {/* NAVBAR FIJO ABAJO */}
       {showMainLayout && (
         <div className="fixed inset-x-0 bottom-0 z-50">
           <NavStick />
